d68248399a4c240a78c7181ec8742ee68321085f
unknown <dgratiy@gmail.com>
2023-12-09 14:43:05 -0800

    flipping sprite yahoo

--- Sword Fighting Game/src/Player.gd
+++ Sword Fighting Game/src/Player.gd
@@ -5,6 +5,7 @@ const SPEED = 60.0
 const JUMP_VELOCITY = -400.0
 const DAMPER = 0.8
 const AIR_DAMPER = 0.85
+@onready var sprite_2d = $Sprite
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -22,8 +23,12 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
 	if direction:
 		velocity.x += direction * SPEED
 	velocity.x = velocity.x * DAMPER if is_on_floor() else velocity.x * AIR_DAMPER
-
+	
+	
 	move_and_slide()
+	var isLeft = velocity.x < 0
+	sprite_2d.flip_h = isLeft
