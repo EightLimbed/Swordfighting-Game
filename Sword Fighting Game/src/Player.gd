dc0fd4d5f77685c972a7cf1e262d4c68f2c527dd
nikolai <xylempalace@icloud.com>
2023-12-12 16:59:20 -0800

    fork

--- Sword Fighting Game/src/Player.gd
+++ Sword Fighting Game/src/Player.gd
@@ -19,32 +19,42 @@ var gravity: int = 0 # ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta: float) -> void:
 	move_and_slide()
+	do_gravity(delta)
+	do_fork()
+	walk()
+	damper()
+	jump()
 
+func do_gravity(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+func do_fork() -> void:
+	if Input.is_action_just_pressed("LEFT_MOUSE"):
 		if fork == null:
 			fork = FORK.instantiate()
 			var to_mouse = Vector2(get_global_mouse_position().x - position.x, get_global_mouse_position().y - position.y).normalized()
 			fork.position = position + (to_mouse * FORK_OFFSET)
+			fork.look_at(get_global_mouse_position())
 			get_node(".").get_parent().add_child(fork)
-			fork.velocity = to_mouse * GRAPPLE_SPEED
-			
+			fork.velocity = to_mouse * GRAPPLE_SPEED	
 	if not fork == null and fork.is_attached():
 		var diff = Vector2(fork.position.x - position.x, fork.position.y - position.y)
 		velocity += diff.normalized() * GRAPPLE_STRENGTH
 		if fork.position.distance_to(position) < GRAPPLE_DIST_MIN:
 			fork.queue_free()
-			
+
+func walk() -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
-	
 	if direction:
 		velocity.x += direction * SPEED
 		sprite_2d.flip_h = direction == -1
+
+func damper() -> void:
 	if fork == null or not fork.is_attached:
 		velocity = velocity * DAMPER if colliding() else velocity * AIR_DAMPER
-		
+
+func jump() -> void:
 	if Input.is_action_just_pressed("ui_accept") and colliding():
 		var diff = Vector2(get_global_mouse_position().x - position.x, get_global_mouse_position().y - position.y)
 		velocity = diff.normalized() * JUMP_VELOCITY
