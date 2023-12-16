b920a0f8f2a0343cec1c3d9247ecfddf1e6d2f77
Dryden Ellis <drydenellis@gmail.com>
2023-12-15 20:32:07 -0500

    cleaned up code

--- Sword Fighting Game/src/ComboRecognition.gd
+++ Sword Fighting Game/src/ComboRecognition.gd
@@ -1,36 +1,38 @@
 extends Node2D
 
-var Points = Array()
-var Size = 0
-var Corners = Array()
-var Length = 101.0
-func find_corners(Coordinates) -> Array:
+var points = Array()
+var size = 0
+var corners = Array()
+var length = 101.0
+func find_corners(coordinates) -> Array:
 	# actual shape recognition here #
-	var Output = Array()
-	return Output
+	var output = Array()
+	return output
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame. #
 func _physics_process(delta: float) -> void:
 	await get_tree().create_timer(0.05).timeout
-	Size = int(Points.size())
+	size = int(points.size())
 	# records coordinates, checks them for shapes, then deletes them #
 	if Input.is_action_pressed("LEFT_MOUSE"):
+		var mouse = get_local_mouse_position()
 		# makes sure the distance between new point and old point is not too small (reduces lag) #
-		if Size > 1:
-			Length = sqrt(((Points[Size-1].x-get_local_mouse_position().x)**2.0)+((Points[Size-1].y-get_local_mouse_position().y)**2.0))
-			if Length > 10:
+		if size > 1:
+			length = sqrt(((points[size-1].x-mouse.x)**2.0)+((points[size-1].y-mouse.y)**2.0))
+			if length > 10:
 				# adds points to list, then finds best corners of shape #
-				Points.append(get_local_mouse_position())
-				Corners = find_corners(Points)
+				points.append(mouse)
+				corners = find_corners(points)
 		else:
-			Points.clear()
-			Points.append(get_local_mouse_position())
-			Points.append(get_local_mouse_position())
+			points.clear()
+			points.append(mouse)
+			points.append(mouse)
 	elif Input.is_action_just_released("LEFT_MOUSE"):
-		Points.clear()
+		points.clear()
 	queue_redraw()
 func _draw() -> void:
 	# draws lines from coordinates #
-	if Size > 1:
-		draw_polyline(Points, Color("#f030a3"), 3)
-		draw_polyline(Corners, Color("#f030a3"), 3)
+	if size > 1:
+		draw_polyline(points, Color("#f030a3"), 3)
+	if corners.size() > 1:
+		draw_polyline(corners, Color("#f030a3"), 3)
